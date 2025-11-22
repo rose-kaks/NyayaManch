0e9ff9381ac0804db39a5936fa6b5c3f733192b9
kajal-082 <kajal082btcse23@igdtuw.ac.in>
2025-11-22 09:53:40 +0530

    added case strength page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css" ;
 
 import Header from "./components/Header";
 import SidebarLayout from "./Layouts/SideBarLayout";
